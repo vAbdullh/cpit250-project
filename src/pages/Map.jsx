a9c102a36e7fc1ed65da2df3271e703f4cb11a08
vAbdullh <abdullah.mansour2@gmail.com>
2024-11-19 20:15:35 +0300

    fix issue

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,9 +1,8 @@
 // FullScreenMap.js
-import React, { useEffect } from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-routing-machine";
-import L from "leaflet";
 
 const Map = () => {
     return (
@@ -12,7 +11,7 @@ const Map = () => {
                 center={[24.7136, 46.6753]}
                 zoom={5}
                 style={{ height: "100vh", width: "100vw" }}
-                zoomControl={false} // Disables zoom controls
+                zoomControl={false}
             >
                 <TileLayer
                     url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
